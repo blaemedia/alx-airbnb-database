d517249af7d2a23e218e97e78a70f9cba9cb6ce3
blaemedia <47524760+blaemedia@users.noreply.github.com>
2025-10-28 02:28:07 +0100

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,19 @@
+SELECT property_id, property_name
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+
+
+SELECT user_id, user_name
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
 
